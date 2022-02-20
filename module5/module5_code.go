eec6577d9544ad4c861499c9cdf43855742db0d5
WaleedMagdy <waleed.magdy.k@n-go.co>
2022-02-20 23:03:13 +0200

    .

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,12 +1,13 @@
 package module5
 
+/*
 import (
 	"fmt"
 	"net/http"
 )
-
+*/
 // GetExampleDotCom uses the "net/http" package to send a GET request to example.com
-
+/*
 func GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 	if err != nil {
@@ -15,3 +16,4 @@ func GetExampleDotCom() {
 
 	defer resp.Body.Close()
 }
+*/
